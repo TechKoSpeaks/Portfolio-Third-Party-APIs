9c1de6fa24edd6bff796216c390fd80a23c7c845
Travis Ko <tkospeaks@gmail.com>
2020-12-17 21:11:21 -0800

    added id's in for text area on HTML to add in functions that will pull info, added in the variables for ensuing jQuery and JSON script.

---
+++ script.js
@@ -0,0 +1,12 @@
+// This section defines all VARIABLES going to be used
+var nineAm = $("#9am");
+var tenAm = $("#10am");
+var elevenAm = $("#11am");
+var twelvePm = $("#12pm");
+var onePm = $("#13pm");
+var twoPm = $("#14pm");
+var threePm = $("#15pm");
+var fourPm = $("#16pm");
+var fivePm = $("#17pm");
+var hourSpan;
+var userInput;
