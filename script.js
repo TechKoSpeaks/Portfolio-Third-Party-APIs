91e00fa0cef848f6d1a7d5bdf47c660b63872444
Travis Ko <tkospeaks@gmail.com>
2020-12-17 21:18:50 -0800

    added in style.css to link in script for styling the blocks with colors, created the for loop for changing the colors into separate blocks based on the time presented. Added current day information pulling from the Moment.js script.

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ var fourPm = $("#16pm");
 var fivePm = $("#17pm");
 var hourSpan;
 var userInput;
+
+
+// Function for setting colors assigned to hours of day within scheduler
+$(function () {
+    // Adds current day to currentDay id in HTML, uses moment js to pull date and time script and set the element for current day
+    var currentdayEl = $("#currentDay");
+    currentdayEl.text(moment().format("MMMM Do YYYY, h:mm a"));
+    var currentHourTime = parseInt(moment().format('k'));
+    console.log(currentdayEl);
+
+
+    // Change color with a for loop, looping through each of the section blocks (9AM-5PM blocks)
+    // and setting appropriate color based on time of day (with row block past, present and future)
+    for (var hourCount = 9; hourCount < 18; hourCount++) {
+        var hourgrabEl = $("#hour-" + hourCount);
+        if(hourCount < currentHourTime) {
+            hourgrabEl.attr("class","row time-block past");
+        }
+        else if(hourCount === currentHourTime) {
+            hourgrabEl.attr("class","row time-block present");
+        }
+        else {
+            hourgrabEl.attr("class","row time-block future");
+        }
+    }
+
+}
